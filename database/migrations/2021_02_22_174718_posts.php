63f76002f9d9f23486f9d63185e03ae64b2fee80
Dylan Murphy <dylan.murphy7799@gmail.com>
2025-03-23 22:26:38 +0000

    Update 2021_02_22_174718_posts.php

--- database/migrations/2021_02_22_174718_posts.php
+++ database/migrations/2021_02_22_174718_posts.php
@@ -4,19 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class Posts extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->dropColumn('category');
+        Schema::create('posts', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('slug');
+            $table->string('title');
+            $table->longText('description');
+            $table->string('image_path');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->string('category');
-        });
+        //
     }
-};
+}
